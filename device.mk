765af82a043d89f069303573ffe2b94040d07a27
K9100ii <k9100ii.ki@gmail.com>
2023-10-21 19:16:22 +0100

    a40: Initial device tree
    Initially derived from the device tree at:
    https://github.com/SamarV-121/android_device_samsung_m20lte
    
    Boot image setup taken from the tree at:
    https://github.com/LineageOS/android_device_samsung_exynos9820-common
    
    Change-Id: I106a7ace9cd911be10011e9432bd660329c7395b

---
+++ device.mk
@@ -0,0 +1,30 @@
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Inherit common device configuration
+$(call inherit-product, device/samsung/universal7904-common/universal7904-common.mk)
+
+$(call inherit-product, vendor/samsung/a40/a40-vendor.mk)
+
+# Init
+PRODUCT_PACKAGES += \
+    fstab.exynos7904 \
+    init.target.rc \
+    init.baseband.rc
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0-service.samsung \
+    libkeymaster4_1support.vendor
+
+# NFC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.product.hardware.sku=NFC
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.exynos7904
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
---
+++ lineage_a40.mk
@@ -0,0 +1,23 @@
+# Copyright (C) 2023 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+# Inherit device configuration.
+$(call inherit-product, device/samsung/a40/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifiers. These must come after all inclusions.
+PRODUCT_DEVICE := a40
+PRODUCT_NAME := lineage_a40
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-A405FN
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
